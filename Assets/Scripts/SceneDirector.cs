91b3ee1e2a5027eb138a0eb972eec8d8f7386e7e
Putitinh332 <167827772+Putitinh332@users.noreply.github.com>
2025-05-20 21:18:57 -0500

    Functioning Scene Transitions
    start/exit button works

---
+++ Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : MonoBehaviour
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
